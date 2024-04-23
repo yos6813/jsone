b46f73d1cafd44468207926c343ceb7723e1e866
jina <yyos6813@gmail.com>
2024-04-23 22:11:44 +0900

    대시보드 및 메인 메뉴 서브메뉴 수정

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -2,6 +2,11 @@
     $(document).ready(function(){
         $('.btn-close').click(function(){
             history.back();
-        })
+        });
+
+        $('.second-nav').hide();
+        $('.sec-nav').click(function(){
+            $('.second-nav').slideToggle();
+        });
     });
 })(jQuery)
